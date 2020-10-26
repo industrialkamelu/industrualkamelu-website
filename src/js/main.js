1378aaeb14766d3433237317329700874ce7b066
Hugo Vilchis <havilchis@outlook.com>
2020-10-25 22:31:32 -0600

    Avances del día

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
 import '../scss/main.scss'
 
+
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 
 
-// ======== NAVBAR ===========================
+
+
+// ===========================================
+// **************  NAVBAR  *******************
+// ===========================================
 $(function () {
+  // Show after hero image
   $(document).scroll(function () {
     var $nav = $(".navbar");
     $nav.toggleClass('navbar-scrolled', $(this).scrollTop() > $nav.height());
+  });
+
+  // Toggle in mobile version
+  let mainNav = document.getElementById('js-menu');
+  let navBarToggle = document.getElementById('js-navbar-toggle');
+  navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('navbar-links-active');
+  });
 });
-});
+
+// ===========================================
+
 
