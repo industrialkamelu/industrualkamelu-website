803b16033fa5c4790b33b88c5bc7746a1f0b26a1
Hugo Vilchis <havilchis@outlook.com>
2020-11-08 23:30:34 -0600

    Fix responive

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -16166,7 +16166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62275" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50802" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
